12dedac9fc58082ee00783a5bc9b273a6db43e21
shreyas4545 <shreyasjakati66@gmail.com>
2023-05-24 02:30:42 +0530

    Changes

--- src/utils/apiConstants.js
+++ src/utils/apiConstants.js
@@ -1,6 +1,6 @@
 
 //Login api
 
-export const BASE_URL = "https://wflow-backend-jlqi7r1b3-shreyas4545.vercel.app/"
+export const BASE_URL = "https://wflow-backend-1kv1silzg-shreyas4545.vercel.app/"
 
 export const LOGIN = `${BASE_URL}api/user/login`;
